8fcf121a1b8b3f26bc568df607d090991d9d8b17
Josh Chianelli <joshchianelli@gmail.com>
2016-01-17 01:20:52 -0600

    Added key features.

--- src/me/jchianelli7/Mine4Me/Miner.java
+++ src/me/jchianelli7/Mine4Me/Miner.java
@@ -2,13 +2,17 @@ package me.jchianelli7.Mine4Me;
 
 import java.awt.AWTException;
 import java.awt.CheckboxMenuItem;
+import java.awt.Graphics;
 import java.awt.Menu;
 import java.awt.MenuItem;
 import java.awt.PopupMenu;
 import java.awt.Robot;
 import java.awt.SystemTray;
 import java.awt.TrayIcon;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
@@ -18,9 +22,8 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 //Using jnativehook for keyboard and mouse listeners.
@@ -29,40 +32,54 @@ import org.jnativehook.NativeHookException;
 import org.jnativehook.keyboard.NativeKeyEvent;
 import org.jnativehook.keyboard.NativeKeyListener;
 
+import javafx.scene.input.KeyCode;
+
 public class Miner implements NativeKeyListener {
-	static Robot bot;
+	static Robot mouseBot;
+	static Robot keyBot;
 	
+	static BufferedImage img = null;
+	static BufferedImage image2 = null;
+	
+	private static boolean listening;
 	private ArrayList<Integer> keysToPress;
+	
+	public Miner() {
+		keysToPress = new ArrayList<Integer>();
+	}
 
 	public static void main(String[] args) throws AWTException {
-		bot = new Robot();
+		mouseBot = new Robot();
+		keyBot = new Robot();
+		
 		System.out.println("Starting Script.");
 		setup();
 	}
 
 	public void PressKeys() throws InterruptedException {
 		for (int key : keysToPress) {
-			bot.keyPress(key);
+			keyBot.keyPress(key);
+			System.out.println("Pressing: " + key);
 		}
 	}
 
 	public void ReleaseKeys() throws InterruptedException {
 		for (int key : keysToPress) {
-			bot.keyRelease(key);
+			keyBot.keyRelease(key);
 		}
 	}
 
+	
 	boolean isPressed = false;
 
 	@Override
 	public void nativeKeyPressed(NativeKeyEvent e) {
-		System.out.println("Pressed");
 		int key = e.getKeyCode();
 		if (key == NativeKeyEvent.VC_PAUSE) {
 			if (isPressed) {
 				System.out.println("Releasing.");
 				try {
-					bot.mouseRelease(InputEvent.BUTTON1_MASK);
+					//mouseBot.mouseRelease(InputEvent.BUTTON1_MASK);
 					ReleaseKeys();
 				} catch (InterruptedException e1) {
 					e1.printStackTrace();
@@ -70,13 +87,17 @@ public class Miner implements NativeKeyListener {
 			} else {
 				System.out.println("Pressing.");
 				try {
-					bot.mousePress(InputEvent.BUTTON1_MASK);
+					//mouseBot.mousePress(InputEvent.BUTTON1_MASK);
 					PressKeys();
 				} catch (InterruptedException e1) {
 					e1.printStackTrace();
 				}
 			}
 			isPressed = !isPressed;
+		} else {
+			if(listening) {
+				keysToPress.add(e.getRawCode());
+			}
 		}
 	}
 
@@ -102,8 +123,7 @@ public class Miner implements NativeKeyListener {
 		}
 		final PopupMenu popup = new PopupMenu();
 
-		BufferedImage img = null;
-		BufferedImage image2 = null;
+
 		try {
 			img = ImageIO.read(ClassLoader.getSystemResource("imgs/toneta.jpg"));
 			image2 = ImageIO.read(ClassLoader.getSystemResource("imgs/toneta2.jpg"));
@@ -149,16 +169,38 @@ public class Miner implements NativeKeyListener {
 
 		JFrame frame = new JFrame("Mine4Me");
 
-		JPanel panel = new JPanel();
+		JPanel panel = new JPanel() {
+			@Override
+			protected void paintComponent(Graphics g) {
+				super.paintComponent(g);
+				g.drawImage(image2, 0, 0, null);
+			}
+		};
+		
+	
+		
+		JButton button = new JButton();
+		button.setText("Click To Add Key");
+		button.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				listening = !listening;
+				if(listening) {
+					((JButton) e.getSource()).setText("Listening");
+				} else {
+					((JButton) e.getSource()).setText("Click To Add Key");
+				}
+			}
+		});
+		panel.add(button);
+		
 		frame.add(panel);
 
 		frame.setIconImage(img);
-		frame.add(new JLabel(new ImageIcon(image2)));
 
 		frame.setSize(image2.getWidth(), image2.getHeight());
-
-		frame.repaint();
-
+		frame.setResizable(false);
 		frame.addWindowListener(new WindowAdapter() {
 
 			@Override
@@ -171,6 +213,7 @@ public class Miner implements NativeKeyListener {
 				}
 				System.exit(1);
 			}
+			
 		});
 		frame.setVisible(true);
 
